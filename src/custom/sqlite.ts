a19ec4dcf6f04932029694f478b5691b823b1704
wxn0brP <toja.filiper@gmail.com>
2026-05-17 20:33:49 +0200

    refactor: sqlite

--- src/custom/sqlite.ts
+++ src/custom/sqlite.ts
@@ -1,6 +1,5 @@
 export async function init(action: any) {
-    const schemas = [
-        `
+    const stmt = await action._prepare(`
 CREATE TABLE IF NOT EXISTS users (
     _id TEXT PRIMARY KEY,
     name TEXT,
@@ -8,19 +7,14 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT,
     isActive INTEGER,
     createdAt INTEGER
-)`,
-        `
+);
 CREATE TABLE IF NOT EXISTS posts (
     _id TEXT PRIMARY KEY,
     title TEXT,
     author TEXT,
     views INTEGER,
     createdAt INTEGER
-)`,
-    ];
-
-    for (const sql of schemas) {
-        const stmt = await action._prepare(sql);
-        await stmt.run();
-    }
+);
+`);
+    await stmt.run();
 }
